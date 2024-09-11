2ea904101df9c91e801ddf58c8ee70287e650169
KevinLi19 <kevin.li@georgiancollege.ca>
2024-09-10 23:45:50 -0400

    Copied the Model class

---
+++ src/main/java/com/example/demojavafx/MyTreasure.java
@@ -0,0 +1,40 @@
+package com.example.demojavafx;
+
+public class MyTreasure {
+    private String name;
+    private String location;
+    private String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location ;
+    }
+
+    public void setLocation(String location) {
+        if (location.contains("xxx"))
+            throw new IllegalArgumentException("invalid location of xxx.");
+        this.location = location;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        if (!imageUrl.contains("http"))
+            throw new IllegalArgumentException("invalid argument.");
+        this.imageUrl = imageUrl;
+    }
+
+    public String toString()
+    {
+        return "Name:" +name + "; Location:" + location ;
+    }
+}
