f61a4fcf13c6a8315192ab0d63920226603e3d0d
KevinLi19 <kevin.li@georgiancollege.ca>
2024-09-10 23:41:08 -0400

    Renamed files/classes

--- src/main/java/com/example/demojavafx/TreasureListController.java
+++ src/main/java/com/example/demojavafx/TreasureListController.java
@@ -3,7 +3,7 @@ package com.example.demojavafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class TreasureListController {
     @FXML
     private Label welcomeText;
 
