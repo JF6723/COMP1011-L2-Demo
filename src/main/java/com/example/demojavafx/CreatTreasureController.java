4e717403044c8d5db730712e392e00a98b78f003
KevinLi19 <kevin.li@georgiancollege.ca>
2024-09-11 16:43:01 -0400

    Added button action

--- src/main/java/com/example/demojavafx/CreatTreasureController.java
+++ src/main/java/com/example/demojavafx/CreatTreasureController.java
@@ -1,4 +1,33 @@
 package com.example.demojavafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
 public class CreatTreasureController {
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private Label labelTreasure;
+
+    @FXML
+    private TextField txtComment;
+
+    @FXML
+    private TextArea txtLocation;
+
+    @FXML
+    private TextField txtName;
+
+
+    public void CreateTreasure(javafx.event.ActionEvent actionEvent) {
+        labelTreasure.setText("Created Treasure");
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demojavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.demojavafx to javafx.fxml;
